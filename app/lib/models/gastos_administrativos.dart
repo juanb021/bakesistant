2b32991cfe5454dfb14e4fa53ad6e278eed08bf8
Juan Rodríguez <juanbrodriguezdb@gmail.com>
2024-09-23 19:51:33 -0400

    Added the option to save information on a database for the expenses

--- app/lib/models/gastos_administrativos.dart
+++ app/lib/models/gastos_administrativos.dart
@@ -1,12 +1,13 @@
 class GastosAdministrativos {
-  const GastosAdministrativos({
+  GastosAdministrativos({
     required this.alquiler,
     required this.depreciacion,
     required this.gVentas,
     required this.nomina,
     required this.papeleria,
     required this.servicios,
-  });
+    double? total,
+  }) : total = total ?? 0;
 
   final double nomina;
   final double depreciacion;
@@ -14,4 +15,5 @@ class GastosAdministrativos {
   final double gVentas;
   final double papeleria;
   final double servicios;
+  final double total;
 }
